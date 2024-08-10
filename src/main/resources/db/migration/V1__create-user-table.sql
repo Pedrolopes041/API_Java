b40aa2537ce7c683fe896138035c4cfd7859908a
Pedro Augusto Lopes Gomes <pedroaugustolopesgomes@gmail.com>
2024-08-10 17:34:37 -0300

    Remove src/main/resources do controle de versão e adiciona ao .gitignore

--- src/main/resources/db/migration/V1__create-user-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE users (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
--- src/main/resources/db/migration/V2__create-event-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE events (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    vacancy INT NOT NULL,
-    date_start TIMESTAMP NOT NULL,
-    date_end TIMESTAMP NOT NULL
-);
